4a8287405bc3d70774eeb4b75e9c625baa448c4f
yudin <brawlakkum@gmail.com>
2023-10-24 21:57:30 +0300

    update

--- database.py
+++ database.py
@@ -5,9 +5,28 @@ import psycopg2
 conn = psycopg2.connect(dbname='users', user='nonanon', 
                         password='admin', host='localhost')
 
-def check_auth(login, passw):
+def check_auth(login):
     try:
         with conn.cursor() as cursor:
-            cursor.execute(f'SELECT {passw} in users WHERE login={login}')
-    except:
-        pass
+            cursor.execute(f'''SELECT password FROM public.users_auth WHERE login = '{login}'
+                           ''')
+            result = cursor.fetchone()
+            if result:
+                return True
+            else:
+                return False
+    except Exception as e:
+        print(e)
+    
+def get_user(login):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(f'''SELECT name, surname FROM public.users_auth WHERE login = '{login}'
+                           ''')
+            credentials = cursor.fetchone()
+            return credentials
+    except Exception as e:
+        return e
+
+
+    #TODO bcrypt
--- main_file.py
+++ main_file.py
@@ -5,44 +5,47 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 
+from database import check_auth, get_user
 
 Builder.load_file('signin.kv')
 
 class EuroplastApp(App):
     def build(self):
-            return AuthorizationPage()
-
-
+        sm = ScreenManager(transition=WipeTransition())
+        sm.add_widget(AuthorizationPage(name='auth_page'))
+        sm.add_widget(MainPage(name='main_page'))
+        return sm
 
 
 class AuthorizationPage(Screen):
 
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.user = self.ids.username_field
+        self.info = self.ids.info
+
+
     def validate_user(self):
-        user = self.ids.username_field
-        pwd = self.ids.pwd_field
-        info = self.ids.info
-
-        uname = user.text
-        passw = pwd.text
-
-        if uname == "" or passw == "":
-            info.text = '[color=#FF0000]Неверный логин или пароль[/color]'
-            return False
+        login = self.user.text
+        if check_auth(login):
+            self.info.text = '[color=#00FF00]Вход выполнен[/color]'
+            self.manager.current = 'main_page'
         else:
-            if uname == "admin" and passw == "admin":
-                info.text= '[color=#00FF00]Вход выполнен![/color]'
-                return True
-            else:
-                info.text = '[color=#FF0000]IНеверный логин или пароль[/color]'
-                return False
+            self.info.text = '[color=#FF0000]Неверный логин или пароль[/color]'
 
 
 class MainPage(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        
     def func_mainpage(self):
-        user = self.ids.username_field
+        info = self.ids.info
+        name, surname = get_user(self.ids.username_field)
+        print(name, surname)
 
+        
 
 if __name__ == '__main__':
     EuroplastApp().run()
