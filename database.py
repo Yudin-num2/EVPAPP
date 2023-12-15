e6fb9138be2f2347c5f71fb584db903158801370
yudin <brawlakkum@gmail.com>
2023-12-15 16:45:53 +0300

    change database on MySQL

--- database.py
+++ database.py
@@ -1,30 +1,36 @@
-import psycopg2
-
+import pymysql
 
 try:
-    conn = psycopg2.connect(dbname='users', user='nonanon', 
-                        password='admin', host='localhost')
+    connection = pymysql.connect(
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    password='qwadratz4323',
+    database='EVPDB',
+    cursorclass=pymysql.cursors.DictCursor
+)
+    print('[INFO] Connection is successfull!')
 except Exception as e:
     print(e)
 
 
 def check_auth(log, passw):
-    try:
-        with conn.cursor() as cursor:
-            query = '''SELECT * FROM public.users_auth WHERE login = %s AND password = %s'''
-            cursor.execute(query, (log, passw,))
-            result = cursor.fetchone()
-            print(result)
-            if result:
-                return True
-            else:
-                return False
-    except Exception as e:
-        print(e)
+    # try:
+    with connection.cursor() as cursor:
+        query = '''SELECT * FROM users_auth WHERE login = %s AND password = %s'''
+        cursor.execute(query, (log, passw,))
+        result = cursor.fetchone()
+        print(result)
+        if result:
+            return True
+        else:
+            return False
+    # except Exception as e:
+    #     print(e)
     
 def get_user(login):
     try:
-        with conn.cursor() as cursor:
+        with connection.cursor() as cursor:
             query = '''SELECT name, surname FROM public.users_auth WHERE login = %s'''
             cursor.execute(query, (login,))
             credentials = cursor.fetchone()
